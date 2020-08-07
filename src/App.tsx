f1139038f585e2a52438c5b88cc59f410ca63156
Azhar7909 <muhammadazharhussain54@gmail.com>
2020-08-07 12:00:57 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 
 function App() {
+  const [renderComponent, setRenderComponent] = useState(false);
+
+    if (renderComponent) 
+      return <HomeScreen />
+  
   return (
-    <div>
-      <HomeScreen />
+    <div className="container">
+      <h1 className="mainHead">WELLCOME TO QUIZ APP</h1>
+      <div style={{ width: '360px', margin: '0 auto' }}>
+        <ul>
+          <li>Total number of questions 5</li>
+          <li>You must give correct answer 3 </li>
+          <li>Passing criteria 60% </li>
+          <li>If you want to dudge your Knowledge then click on Start Button</li>
+        </ul>
+      </div>
+      <div style={{textAlign: 'center',paddingTop:'50px'}}>
+        <Button onClick={()=>setRenderComponent(true)} variant="contained" color="secondary">
+          START QUIZ
+        </Button>
+      </div>
+      {/* <HomeScreen /> */}
     </div>
   );
 }
